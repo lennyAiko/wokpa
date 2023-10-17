e0f784c86d1a9fa9e92e2be0726eb1efb28ba976
lennox charles <lennyaiko17@gmail.com>
2023-10-17 23:04:06 +0100

    chore

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        loader: "akamai",
-        path: "/"
-    }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,28 +12,28 @@ export default function Home() {
         <img src="wokpalogo.png" className='w-16 h-8 shrink-0 top-[4px] left-[15px] relative mb-4' />
 
         <div>
-          <Menu name='Discover' img='sound.png'></Menu>
-          <Menu name='Top 50 podcasts' img='star.png'></Menu>
-          <Menu name='Categories' img='cat.png'></Menu>
-          <Menu name='Add your podcast' img='mic.png'></Menu>
+          <Menu name='Discover' img='/menu/sound.png'></Menu>
+          <Menu name='Top 50 podcasts' img='/menu/star.png'></Menu>
+          <Menu name='Categories' img='/menu/cat.png'></Menu>
+          <Menu name='Add your podcast' img='/menu/mic.png'></Menu>
 
           <p className='font-bold text-xs text-white m-3'>Your Library</p>
 
           <div className="flex flex-row items-center m-3 gap-12">
-            <img src="love.png" alt="" className="cursor-pointer" />
-            <img src="strokes.png" alt="" className="cursor-pointer" />
-            <img src="plus.png" alt="" className="cursor-pointer" />
+            <img src="/menu/love.png" alt="" className="cursor-pointer" />
+            <img src="/menu/strokes.png" alt="" className="cursor-pointer" />
+            <img src="/menu/plus.png" alt="" className="cursor-pointer" />
           </div>
 
-          <Menu name='Your playlist' img='music.png'></Menu>
-          <Menu name='Recently played' img='earphones.png'></Menu>
-          <Menu name='Subscription & Hosts' img='group.png'></Menu>
+          <Menu name='Your playlist' img='/menu/music.png'></Menu>
+          <Menu name='Recently played' img='/menu/earphones.png'></Menu>
+          <Menu name='Subscription & Hosts' img='/menu/group.png'></Menu>
 
           <hr className="w-full my-3 border-0 h-0.5 bg-gradient-to-r from-[#D9D9D99C] to-[#D9D9D900]" />
 
-          <Menu name='Wokpa for podcasters' img='pod.png'></Menu>
-          <Menu name='Wokpa for advertisers' img='ham.png'></Menu>
-          <Menu name='Read our blogs' img='blog.png'></Menu>
+          <Menu name='Wokpa for podcasters' img='/menu/pod.png'></Menu>
+          <Menu name='Wokpa for advertisers' img='/menu/ham.png'></Menu>
+          <Menu name='Read our blogs' img='/menu/blog.png'></Menu>
         </div>
 
       </div>
